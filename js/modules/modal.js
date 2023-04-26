da9711f02d41b35632bb4bd8e417716aab109119
JULIAVALERIO <juliav286@gmail.com>
2023-04-26 09:34:42 -0300

    adiionando novo modulo tooltip

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,5 +1,4 @@
 export default function initModal() {
-console.log('oi')
   const botaoAbrir = document.querySelector('[data-modal="abrir"]');
   const botaoFechar = document.querySelector('[data-modal="fechar"]');
   const containerModal = document.querySelector('[data-modal="container"]');
